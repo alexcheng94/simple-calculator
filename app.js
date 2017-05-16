eab056f2b381d08a6fb9bb433dc3573c723c55b5
alexcheng94 <alexcheng94@126.com>
2017-05-16 20:13:16 +0800

    improved functionality.

--- app.js
+++ app.js
@@ -3,57 +3,44 @@ var placeHolder = $('#result')[0];
 var $clearButton = $('#clear');
 var $equalButton = $('#equal');
 var $hint = $('.hint');
-var $calcButton = $('.calc-op');
+var $acButton = $('#allClear');
 
 //check the length of input
-function checkPointCount(){
-	var pointCount = placeHolder.innerHTML.split('.').length-1;
-	if (pointCount > 1) {
-		$hint[0].innerHTML = "Oops, you enterd more than one decimal point in a number!";
-		return true;
-	} else{
-		return false;
-	}
-}
-// check the number of decimal points
 function checkNumberLength(){
 	var numberLength = placeHolder.innerHTML.length;
 	if (numberLength > 48) {
 		$hint[0].innerHTML = "I can only show 48 digits D:";
-		return true;
-	} else{
-		return false;
-	}
-}
-
-function showHint(){
-	if (checkPointCount() || checkNumberLength()) {
 		$hint.show();
-	}
-}
-
-function hideHint(){
-	if (!checkPointCount() && !checkNumberLength()) {
-		$hint.hide();
 	}
 }
 
 $hint.hide();
 
 //input a number to the result window
-$button.click(function(event) {
-	placeHolder.innerHTML += ($(this).attr('value'));
-	showHint();
+$button.click(function() {
+	placeHolder.innerHTML += this.value;
+	checkNumberLength();
 });
 
 //Clear the last character in the result window when CE button is clicked
-$clearButton.click(function(event) {
+$clearButton.click(function() {
 	var length = placeHolder.innerHTML.length;
 	placeHolder.innerHTML = placeHolder.innerHTML.slice(0,length-1);
-	hideHint();
+	$hint.hide();
 });
 
-$equalButton.click(function(event) {
-	result = eval(placeHolder.innerHTML);
-	placeHolder.innerHTML = result;
+//Clear all 
+$acButton.click(function(event) {
+	placeHolder.innerHTML = "";
+	$hint.hide();
+});
+
+$equalButton.click(function() {
+	try {
+		result = eval(placeHolder.innerHTML);
+		placeHolder.innerHTML = result;
+	} catch(e) {
+		$hint[0].innerHTML = "There's an error with your input, please try again.";
+		$hint.show();
+	}
 });
